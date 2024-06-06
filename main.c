b3916e9082d64f01a98cb17208808b03048eeb52
PedroAntonio2 <145236656+PedroAntonio2@users.noreply.github.com>
2024-06-06 11:57:31 -0300

    Updating code for testing start pulse

--- main.c
+++ main.c
@@ -1,98 +1,101 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h> /*biblioteca para o uso das rotinas de
- _delay_ms() e _delay_us()*/
+#include <util/delay.h>
 
 #define cpl_bit(y,bit) (y^=(1<<bit))
-#define set_bit(Y,bit_x) (Y|=(1<<bit_x)) 
+#define set_bit(Y,bit_x) (Y|=(1<<bit_x))
 #define clr_bit(Y,bit_x) (Y&=~(1<<bit_x))
-#define master_clock PB5
-#define porta_start PB4
+#define master_clock PB4
+#define porta_start PB5
 #define LED PB3
 
-int main_clock = 0;
-int led_turn_on_count = 18;
-int count_measure = 0;
+volatile int main_clock = 0;
+volatile int led_turn_on_count = 18;
+volatile int count_measure = 0;
 
-int measure_limit = 111;
-int init = 0;
+volatile int measure_limit = 111;
+volatile int inicial = 0;
 
-int start = 0;
-int reset_start = 0;
-int turn_led = 0;
-int reset_cycle = 0;
-int measure_cycle = 0;
+volatile int start = 0;
+volatile int reset_start = 0;
+volatile int turn_led = 0;
+volatile int reset_cycle = 0;
+volatile int measure_cycle = 0;
 
-
-//----------------------------------------------------------------------------------
-ISR(TIMER0_OVF_vect)
+//---------------------------------------------------------------------------------
+ISR(TIMER2_COMPA_vect)
 {
-	//count == 2 f = 31250Hz
-	cpl_bit(PORTB, master_clock);
-	main_clock = ~main_clock;
-	
-	if(init == 2){
+  //f = 31250Hz
+  cpl_bit(PORTB, master_clock);
+  main_clock = main_clock^1;
+ 
+  if(inicial == 2){
+    if(main_clock == 1){
+		start = 1;
 		reset_start = 1;
-		if(main_clock == 1){
-			if(reset_cycle == 1){
-				led_turn_on_count--;
-			}
-			if(led_turn_on_count == 0){
-				turn_led = 1;
-				measure_cycle = 1;
-				reset_cycle = 0;
-				led_turn_on_count = 18;
-			}
-			if(turn_led == 1){
-				set_bit(PORTB, LED);
-			}
-			if(measure_cycle == 1){
-				count_measure++;
-			}
-			if(count_measure == measure_limit + 1){
-				start = 1;
-			}
-			if(count_measure == measure_limit + 2){
-				measure_cycle = 0;
-				turn_led = 0;
-			}
-			if(turn_led == 0){
-				clr_bit(PORTB, LED);
-			}
-		}
-	}
-	if((init < 2) && (main_clock == 1)){
-	init = init + 1;
-	start = 1;
-	}
+      if(reset_cycle == 1){
+        start = 0;
+        led_turn_on_count--;
+      }
+      if(led_turn_on_count == 0){
+        turn_led = 1;
+        measure_cycle = 1;
+        reset_cycle = 0;
+        start = 0;
+        led_turn_on_count = 18;
+      }
+      if(turn_led == 1){
+        set_bit(PORTB, LED);
+      }
+      if(measure_cycle == 1){
+        start = 0;
+        count_measure++;
+      }
+      if(count_measure == measure_limit + 1){
+        start = 1;
+      }
+      if(count_measure == measure_limit + 2){
+        measure_cycle = 0;
+        turn_led = 0;
+      }
+      if(turn_led == 0){
+        clr_bit(PORTB, LED);
+      }
+    }
+  }
+  if((inicial < 2) && (main_clock == 1)){
+  inicial = inicial + 1;
+  }
 }
 
-
-
-int main(void)
-{
+int main(void){
 	DDRB  = 0b00111000;
 	PORTB = 0b11000111;
 
-	TCCR0B = 0b00000001;
-	TCNT0 = 0;
-	
-	TIMSK0 = 1<<TOIE0;
+	// Configurando o Timer 2 no modo CTC
+	TCCR2A = (1 << WGM21); // Configura o Timer 2 para o modo CTC (Clear Timer on Compare Match)
+	TCCR2B = (1 << CS20);  // Prescaler de 1
+
+	OCR2A = 255; // Valor de comparação para gerar uma interrupção a cada 16us (62.5ns * 256 = 16us)
+
+	TIMSK2 = (1 << OCIE2A); // Habilitando a interrupção de comparação do Timer 2
 	sei();
 
+
 	while(1){
+		ISR(TIMER2_COMPA_vect);
 		if(start == 1){
 			_delay_us(16);
 			set_bit(PORTB, porta_start);
+			reset_cycle = 1;
 			reset_start = 0;
 			start = 0;
 		}
-		if(reset_start == 1){
+		else if(reset_start == 1){
 			_delay_us(16);
 			clr_bit(PORTB, porta_start);
 		}
-		
-		ISR(TIMER0_OVF_vect);
+
 	}
 }
